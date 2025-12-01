ce97abfbb355c5736424f49e529b956831173702
Joao Kuster <kusterzito@gmail.com>
2025-12-01 14:16:38 -0300

    feat: dropdown component finished

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -1,21 +1,43 @@
 import { useState } from 'react'
 import './dropdown.css'
 
+interface SelectItemProps {
+  id: number | string;
+  label: string;
+  value: string;
+}
+
+const selectItems: SelectItemProps[] = [
+  { id: 1, label: 'Apple', value: 'Apple fruit' },
+  { id: 2, label: 'Grape', value: 'Grape' },
+  { id: 3, label: 'Banana', value: 'Banana fruit' },
+]
+
 export function Dropdown() {
   const [isOpen, setIsOpen] = useState(false)
+  const [selected, setSelected] = useState<SelectItemProps | null>(null)
+
+  const select = (item: SelectItemProps) => {
+    if (!item || item.id === selected?.id) return
+    setSelected(item)
+    setIsOpen(false)
+  }
 
   return (
     <div className="dropdown__area">
       <h2>Dropdown</h2>
       <div className='dropdown'>
         <button onClick={() => setIsOpen(!isOpen)} className="dropdown__trigger">
-          Select an option
+          {selected ? selected.label : 'Select an option'}
         </button>
         {isOpen && (
           <ul className="dropdown__content">
-            <li className="dropdown__item">Item 1</li>
-            <li className="dropdown__item">Item 2</li>
-            <li className="dropdown__item">Item 3</li>
+            {selectItems.map((item) => (
+              <li key={item.id} onClick={() => select(item)} className="dropdown__item">
+                {selected?.id === item.id && <span className='dropdown__item--active'></span>}
+                {item.label}
+              </li>
+            ))}
           </ul>
         )}
       </div>
